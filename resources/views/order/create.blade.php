e0ad6a286b356f555bfa093787ca6cea476393de
scchethu <psi.palmate@gmail.com>
2021-09-19 18:10:55 +0530

    imf added

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -7,31 +7,39 @@
 @stop
 
 @section('content')
-<div>
-    <form action="{{route('orders.store')}}" method="post">
-        @csrf
-        <input type="hidden" name="fabric_id" value="{{$fabric_id}}" >
-        <input type="hidden" name="user_id" value="{{\Illuminate\Support\Facades\Auth::id()}}" >
-        <div class="form-group">
-            <label for="">Measurements</label>
-            <textarea  required class="form-control" name="measurements" id="" cols="30" rows="5" placeholder="Enter measurements"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="">Delivery Address</label>
-            <textarea  required class="form-control" name="address" id="" cols="30" rows="5" placeholder="Enter Address"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="">Tailor</label>
-            <select class="form-control" name="tailor_id" id="">
-                @foreach(\App\Models\Tailor::all() as $tailor)
-                    <option value="{{$tailor->id}}">{{$tailor->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Place Order">
-        </div>
-    </form>
+<div class="row">
+    <div class="col-md-4 card p-3 sticky-top">
+        <form action="{{route('orders.store')}}" method="post">
+            @csrf
+            <input type="hidden" name="fabric_id" value="{{$fabric_id}}" >
+            <input type="hidden" name="user_id" value="{{\Illuminate\Support\Facades\Auth::id()}}" >
+            <div class="form-group">
+                <label for="">Measurements</label>
+                <textarea  required class="form-control" name="measurements" id="" cols="30" rows="5" placeholder="Enter measurements"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="">Delivery Address</label>
+                <textarea  required class="form-control" name="address" id="" cols="30" rows="5" placeholder="Enter Address"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="">Tailor</label>
+                <select class="form-control" name="tailor_id" id="">
+                    @foreach(\App\Models\Tailor::all() as $tailor)
+                        <option value="{{$tailor->id}}">{{$tailor->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Place Order">
+            </div>
+        </form>
+    </div>
+    <div class="col-md-8">
+        <h2>Standard Measurements</h2>
+        <img class="img-fluid" src="{{asset('img/20210301_124536-01.jpeg')}}" alt="">
+        <img class="img-fluid" src="{{asset('img/20210301_124613-01.jpeg')}}" alt="">
+        <img class="img-fluid" src="{{asset('img/1632031820490_1632031644096_20210301_124602-01.jpeg')}}" alt="">
+    </div>
 </div>
 @stop
 
